98b329ae2f540efe12dd4e86d9e8d8447b69d2d8
IT-Tejas <65008967+IT-Tejas@users.noreply.github.com>
2020-10-07 12:14:54 +0530

    Youtube_videos_downloader.py

--- yt.py
+++ yt.py
@@ -1,15 +1,18 @@
+print("""
+************************************TECH_TEJAS******************************************
+""")
 try:
 	try:
-		import pytube
+		from pytube import YouTube
 	except:
 		print("\nNeed To Download Some Files")
 		inp=input("\nShould I Download (y/n): ")
 		inp=inp.lower()
 		if inp=='y':
 			try:
-				import os
-				os.system("pip install pytube")
-				os.system("pip install pytube3")
+				import pip
+				pip.main(["install", "pytube"])
+				pip.main(["install", "pytube3"])
 				exit()
 			except:
 				print("Please Check Your Internet Connection!")
@@ -18,11 +21,23 @@ try:
 			exit()
 	link = input('\nEnter the link: ')
 	print("\nPlease Wait....")
-	yt = pytube.YouTube(link)
-	stream = yt.streams.first()
+	yt = YouTube(link)
+	print("\n***********************All Video Streams************************\n")
+	stre = yt.streams.all()
+	n=1
+	for i in stre:
+	    print(f"{n})    {i}")
+	    n+=1
+	qua=int(input("\nEnter Video Index Number: "))
+	video=stre[qua-1]
+	print(video)
 	print("\nDownloading Started!")
-	stream.download()
+	print("\n\n****************************Video Details*************************")
+	print("\nTitle: "+yt.title)
+	print("\nThumbnail: "+yt.thumbnail_url)
+	video.download()
 	print('\nDownload Completed!')
-except:
+except Exception as e:
 	print("\nSomething Went Wrong!")
+#	print(e)
 	exit()
